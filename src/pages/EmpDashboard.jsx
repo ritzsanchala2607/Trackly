d27b7dd0b21de8aaca0759c214e14793e3d3e223
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2025-04-03 10:38:48 +0530

    Added btn in employee dashboard to route to calendar page from dashboard

--- src/pages/EmpDashboard.jsx
+++ src/pages/EmpDashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { GoPrimitiveDot } from 'react-icons/go';
 import { IoIosMore } from 'react-icons/io';
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+import { Link } from "react-router-dom";
 
 import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
 import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData, employeesData, employeesGrid } from '../data/dummy';
@@ -26,8 +27,8 @@ const EmpDashboard =() => {
   return (
     <div className="mt-7">
       <div className="flex flex-wrap lg:flex-nowrap justify-end gap-x-4 mr-5">
-          <Button color="white" bgColor={currentColor} text="Add Lead" borderRadius="10px" />
-          <Button color="white" bgColor={currentColor} text="Add Emp" borderRadius="10px" />
+          <Link to="/addlead"><Button color="white" bgColor={currentColor} text="Add Lead" borderRadius="10px" /></Link>
+          <Link to="/calendar"><Button color="white" bgColor={currentColor} text="My Calender" borderRadius="10px" /></Link>
       </div>
       <div className="flex m-3 flex-wrap justify-evenly gap-1 items-center">
       {earningData.map((item) => (
